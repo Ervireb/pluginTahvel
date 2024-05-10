12ed1c65e259ee69dd2397188fab3b925e0640f2
Ervireb <77449663+Ervireb@users.noreply.github.com>
2024-05-10 15:28:22 +0300

    ver5.111
    Letters now with small Spaces in sidebar

--- pluginT.js
+++ pluginT.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         LwSp to sidebar
+// @name         L w smol Sp sidebar
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.5.11111
 // @description  Scroll menu to navigate to specific letters
 // @author       er+sky
 // @match        https://tahvel.edu.ee/
@@ -11,7 +11,10 @@
 (function() {
     'use strict';
 
+    //сделать адаптивные пробелы можн ч привязку к нескольким шир экрана
+    // положить всё в md-content с отступом ~64 пикселей. разн на кажд шир экрана
     // Delay the execution to ensure all elements are loaded before processing
+  function runUserScript() {
     setTimeout(() => {
 		// Выбираем все элементы с классом schoolTileFooter
 		const elements = document.querySelectorAll('.schoolTileFooter');
@@ -43,21 +46,25 @@
 
         const menuContainer = document.createElement('div');
         menuContainer.style.position = 'fixed';
-        menuContainer.style.top = '50%';
-        menuContainer.style.right = '10px';
+        menuContainer.style.top = '54%';
+        menuContainer.style.right = '3px';
+        menuContainer.style.height = '85%';
         menuContainer.style.padding = '2px';
         menuContainer.style.textAlign = 'center';
         menuContainer.style.transform = 'translateY(-50%)';
+//        menuContainer.style.background = 'red';
+        menuContainer.id = 'menuContainer';
         menuContainer.style.zIndex = 9999;
 
         spacedLetters.forEach(letter => {
             const menuItem = document.createElement('div');
             menuItem.textContent = letter;
-            menuItem.style.cssText = 'cursor:pointer; padding:0 12px; margin-bottom:5px;';
+            menuItem.style.cssText = 'cursor:pointer; padding:8px 17px; margin-bottom:1px; border-radius: 10px 50px 50px 10px;';
             menuItem.style.backgroundColor = 'rgba(190, 190, 190, 0.6)';
             if(letter == isNbsp){
                 menuItem.style.backgroundColor = 'rgba(255, 255, 255, 0)';
                 menuItem.style.marginBottom = '0px';
+                menuItem.style.padding = '0px';
                 menuItem.style.height = '10px';
             }
 
@@ -80,19 +87,62 @@
             });
             menuContainer.appendChild(menuItem);
         });
-        document.body.appendChild(menuContainer);
+        document.getElementById('content-and-sidenav-wrapper').appendChild(menuContainer);
 
 
 
-
-        const style = document.createElement('style');
-        style.innerHTML = `
-.fade-out {
-    animation: fadeOut 1s ease;
+	const style = document.createElement('style');
+	style.innerHTML = `
+.smth {
+    colour: cyan;
 }
 
 `;
-        document.head.appendChild(style);
+	document.head.appendChild(style);
 
-    }, 400);
+	}, 400);
+  }
+
+
+
+
+
+function adjustHeight() {
+    const menuSidebar = document.getElementById('menuContainer');
+    if (menuSidebar) {
+        if (window.innerWidth < 900) {
+            menuSidebar.style.height = '94%';
+        } else {
+            menuSidebar.style.height = '85%';
+        }
+    } else {
+        console.log('Элемент с id "menuContainer" не найден.');
+    }
+}
+adjustHeight();			// Обработчик изменения размеров окна
+window.addEventListener('resize', adjustHeight);
+
+
+
+
+
+
+// Запуск кода при загрузке страницы
+runUserScript();
+
+// Обработчики событий для повторного запуска кода
+window.addEventListener('popstate', function(event) {
+    if (document.getElementById('menuContainer')) {
+        document.getElementById('menuContainer').remove();
+    } else { runUserScript(); }
+});
+
+window.addEventListener('hashchange', function(event) {
+    if (document.getElementById('menuContainer')) {
+        document.getElementById('menuContainer').remove();
+    } else { runUserScript(); }
+});
+
+
+
 })()
